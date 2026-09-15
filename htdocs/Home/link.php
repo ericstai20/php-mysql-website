8340bc9a683ea9c10658f869b3846f097b56bc7b
ericstai20 <134434654+ericstai20@users.noreply.github.com>
2026-09-15 16:09:54 -0500

    Add files via upload

---
+++ htdocs/Home/link.php
@@ -0,0 +1,13 @@
+<?
+include ('../includes/header.php'); // Include the header file.
+?>
+
+<h2>Related Links</h2>
+<a href="https://www.packers.com" target="_blank">Official Packers Website</a><p>
+<a href="https://www.espn.com/nfl/team/_/name/gb/green-bay-packers" target="_blank">ESPN - Packers Coverage</a><p>
+<a href="https://www.jsonline.com/sports/packers/" target="_blank">Milwaukee Journal Sentinel - Packers News</a><p>
+
+<?
+include ('../includes/footer.php'); //Include the footer file.
+?>
+</parameter>
