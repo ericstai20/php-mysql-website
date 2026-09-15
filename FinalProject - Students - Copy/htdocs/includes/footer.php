6bb4c3401ed4c4becdc5f4fbd91168acdd82dfb1
ericstai20 <134434654+ericstai20@users.noreply.github.com>
2026-09-15 16:10:21 -0500

    Delete FinalProject - Students - Copy/htdocs directory

--- FinalProject - Students - Copy/htdocs/includes/footer.php
+++
@@ -1,7 +0,0 @@
-</td></tr>
-</table>
-<div id="footer" style="background-color:#FFB612; color:#203731; padding:15px; margin-top:30px;">
-		<p>Go Pack Go! | Green Bay Packers Fan Site &copy; 2026</p>
-</div>
-</body>
-</html>
